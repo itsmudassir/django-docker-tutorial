29dd713233cf3e883820eb3ba44554e973132739
Muhammad Mudassir <m.mudassir2015@gmail.com>
2024-11-21 16:31:29 +0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
 # Print Hello
-print("Hello!")
 
 # Get and print the current time
 current_time = datetime.now()
-print("The current time is:", current_time.strftime("%Y-%m-%d %H:%M:%S"))
+print("hello : ", current_time.strftime("%Y-%m-%d %H:%M:%S"))
